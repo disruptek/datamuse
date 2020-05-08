64ee390c5f192e4266d708afe065f22ee4768217
Andy Davidoff <andy+github@disruptek.com>
2020-05-08 18:34:01 -0400

    broken

--- datamuse.nim
+++ datamuse.nim
@@ -1,3 +1,6 @@
+import std/strutils
+import std/macros
+import std/tables
 import std/sequtils
 import std/options
 import std/uri
@@ -9,46 +12,146 @@ import bump
 import rest
 import cutelog
 
+type
+  Lookup* = enum
+    MeansLike = "ml"
+    SoundsLike = "sl"
+    SpelledLike = "sp"
+    Vocabulary = "v"
+    Topics = "topics"
+    LeftContext = "lc"
+    RightContext = "rc"
+    MaximumResults = "max"
+    QueryEcho = "qe"
+
+  Related* = enum
+    jja = "jja"
+    jjb = "jjb"
+    syn = "syn"
+    trg = "trg"
+    ant = "ant"
+    spc = "spc"
+    gen = "gen"
+    com = "com"
+    par = "par"
+    bga = "bga"
+    bgb = "bgb"
+    rhy = "rhy"
+    nry = "nry"
+    hom = "hom"
+    cns = "cns"
+
+  MetaData* = enum
+    Definitions = "d"
+    PartsOfSpeech = "p"
+    SyllableCount = "s"
+    Pronunciation = "r"
+    WordFrequency = "f"
+
+  Suggestion* = enum
+    PrefixHint = "s"
+    MaximumSuggestions = "max"
+    SuggestionVocabulary = "v"
+
+  Result* = object
+    word: string
+    score: int
+    syllables: range[1 .. int.high]
+
+  QueryTable[T] = Table[T, string]
+  QueryKind* = enum
+    Lookups = "/words"
+    Suggestions = "/sug"
+  MuseCall* = object
+    results: seq[Result]
+    case kind: QueryKind
+    of Lookups:
+      lookup: QueryTable[Lookup]
+      metadata: QueryTable[MetaData]
+      related: QueryTable[Related]
+    of Suggestions:
+      suggestion: QueryTable[Suggestion]
+
 proc newDataMuseCall*(url: Uri; name = "datamuse"): RestCall =
   result = RestCall(name: name, meth: HttpGet, url: url)
 
-proc newWordsCall*(args: openArray[tuple[key, val: string]]): RestCall =
+proc newMuseCall*(kind: QueryKind;
+                  args: openArray[tuple[key, val: string]]): RestCall =
   const
-    wordsUrl = "https://api.datamuse.com/words".parseUri
-  result = newDataMuseCall(wordsUrl ? args, name = "datamuse words")
-
-proc newSuggCall*(args: openArray[tuple[key, val: string]]): RestCall =
-  const
-    suggUrl = "https://api.datamuse.com/sug".parseUri
-  result = newDataMuseCall(suggUrl ? args, name = "datamuse suggestions")
-
-proc muse(rel_rhy = ""; rel_trg = ""; rel_jja = ""; rel_jjb = "";
-          sp = ""; ml = ""; sl = ""; sugg = ""; lc = ""; topics = "") =
+    datamuseUrl = "https://api.datamuse.com/".parseUri
   var
-    args = toSeq {
-      "rel_rhy": rel_rhy,
-      "rel_trg": rel_trg,
-      "rel_jja": rel_jja,
-      "rel_jjb": rel_jjb,
-      "s": sugg,
-      "sp": sp,
-      "lc": lc,
-      "ml": ml,
-      "sl": sl,
-      "topics": "",
-    }
-  args.keepItIf it[1] != ""
-  var
-    call =
-      if sugg != "":
-        newSuggCall args
-      else:
-        newWordsCall args
+    args = toSeq args
+    url = datamuseUrl / $kind ? args
+  result = newDataMuseCall(url, name = "datamuse " & $kind)
+
+template newWordsCall*(args: openArray[tuple[key, val: string]]): RestCall =
+  result = Lookups.newMuseCall args
+
+template newSuggCall*(args: openArray[tuple[key, val: string]]): RestCall =
+  result = Suggestions.newMuseCall args
+
+proc addArgument(params: var seq[NimNode]; args: NimNode;
+                  switch: enum): NimNode =
   let
-    request = newRecallable(call)
-    response = request.retried
-    js = parseJson(waitfor response.body)
-  debug js.pretty
+    sw = ident($switch)
+    sws = newStrLitNode($switch)
+  params.add newIdentDefs(sw, ident"string", default = newStrLitNode"")
+  result = quote do:
+    if `sw` != "":
+      `args`.add (`sws`, `sw`)
+
+macro makeMuse(kind: QueryKind; name: string): NimNode =
+  ## generate some cligen entry points that are correct by construction
+  let
+    args = ident"args"
+    call = ident"call"
+    name = postfix(ident($name), "*")
+
+  # setup each procedure
+  var
+    params: seq[NimNode] = @[ident"int"]
+    body = newStmtList()
+  body.add quote do:
+    var
+      `args`: seq[tuple[key, val: string]]
+
+  # build the custom parameter processing per procedure
+  case $kind
+  of "Lookups":
+    for switch in Lookup.items:
+      body.add params.addArgument(args, switch)
+    for switch in MetaData.items:
+      body.add params.addArgument(args, switch)
+    for switch in Related.items:
+      body.add params.addArgument(args, switch)
+    body.add quote do:
+      var `call` = newMuseCall(Lookups, `args`)
+  of "Suggestions":
+    for switch in Suggestion.items:
+      body.add params.addArgument(args, switch)
+    body.add quote do:
+      var `call` = newMuseCall(Suggestions, `args`)
+
+  # parsing results is common to all procedures
+  body.add quote do:
+    let
+      request = newRecallable(`call`)
+      response = request.retried
+      js = parseJson(waitfor response.body)
+    debug js.pretty
+    return 0
+
+  # generate the proc node
+  result = newProc(name, params, body)
+  echo result.repr
+  when false:
+    for result in js.items:
+      `call`.results.add Result(word: result["word"].getStr,
+                               score: result{"score"}.getInt,
+                               syllables: result{"numSyllables"}.getInt)
+
+makeMuse(Suggestions, "suggest")
+makeMuse(Lookups, "lookup")
 
 when isMainModule:
   import cligen
@@ -65,20 +168,4 @@ when isMainModule:
   else:
     clCfg.version = "(unknown version)"
 
-  dispatchCf muse, cmdName = "muse", cf = clCfg
-when false:
-  dispatchCf name, cmdName = "name", cf = clCfg,
-    usage = "Options(opt-arg sep :|=|spc):\n$options",
-    help = {
-      "patch": "increment the patch version field",
-      "minor": "increment the minor version field",
-      "major": "increment the major version field",
-      "dry-run": "just report the projected version",
-      "commit": "also commit any other unstaged changes",
-      "v": "prefix the version tag with an ugly `v`",
-      "nimble": "specify the nimble file to modify",
-      "folder": "specify the location of the nimble file",
-      "release": "also use `hub` to issue a GitHub release",
-      "log-level": "specify Nim logging level",
-      "manual": "manually set the new version to #.#.#",
-    }
+  dispatchMulti [suggest], [lookup], cf = clCfg
